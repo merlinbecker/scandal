72ff04b1904350cee3573f3e87f8f309d073567f
merlinbecker <merlin.becker@googlemail.com>
2019-03-06 21:01:43 +0100

    EXIF auslesen

--- app.js
+++ app.js
@@ -14,14 +14,25 @@
  *
  *
  * @todo: refactor this!
- *
+ * @todo: clean memory up! 
  *
  * **/
 var assets=0;
 
 $(document).ready(function(evt){
 	$("#fileInput").change(function(evt){
-		$("#inputImage").attr("src",URL.createObjectURL(evt.target.files[0]));
+		var file = evt.target.files[0];
+		getOrientation(file, function(orientation) {
+			 var reader = new FileReader();
+			 reader.onload = function(event) {
+				 console.log("Filereader result!");
+				 console.log(event.target.result);
+				 resetOrientation(event.target.result,orientation,function(resetImg){
+					 $("#inputImage").attr("src",resetImg);
+				 });
+			 };
+			 reader.readAsDataURL(file);
+		});
 	});
 });
 function onJsPDFReady(){
@@ -43,6 +54,80 @@ function setAssetLoaded(){
 		do_calculation();
 	}
 }
+function resetOrientation(srcBase64, srcOrientation, callback) {
+	var img = new Image();	
+
+	img.onload = function() {
+  	var width = img.width,
+    		height = img.height,
+        canvas = document.createElement('canvas'),
+	  		ctx = canvas.getContext("2d");
+		
+    // set proper canvas dimensions before transform & export
+		if (4 < srcOrientation && srcOrientation < 9) {
+    	canvas.width = height;
+      canvas.height = width;
+    } else {
+    	canvas.width = width;
+      canvas.height = height;
+    }
+	
+  	// transform context before drawing image
+		switch (srcOrientation) {
+      case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+      case 3: ctx.transform(-1, 0, 0, -1, width, height ); break;
+      case 4: ctx.transform(1, 0, 0, -1, 0, height ); break;
+      case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+      case 6: ctx.transform(0, 1, -1, 0, height , 0); break;
+      case 7: ctx.transform(0, -1, -1, 0, height , width); break;
+      case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+      default: break;
+    }
+
+		// draw image
+    ctx.drawImage(img, 0, 0);
+
+		// export base64
+		callback(canvas.toDataURL());
+  };
+
+	img.src = srcBase64;
+}
+
+function getOrientation(file, callback) {
+  var reader = new FileReader();
+  reader.onload = function(event) {
+    var view = new DataView(event.target.result);
+
+    if (view.getUint16(0, false) != 0xFFD8) return callback(-2);
+
+    var length = view.byteLength,
+        offset = 2;
+
+    while (offset < length) {
+      var marker = view.getUint16(offset, false);
+      offset += 2;
+
+      if (marker == 0xFFE1) {
+        if (view.getUint32(offset += 2, false) != 0x45786966) {
+          return callback(-1);
+        }
+        var little = view.getUint16(offset += 6, false) == 0x4949;
+        offset += view.getUint32(offset + 4, little);
+        var tags = view.getUint16(offset, little);
+        offset += 2;
+
+        for (var i = 0; i < tags; i++)
+          if (view.getUint16(offset + (i * 12), little) == 0x0112)
+            return callback(view.getUint16(offset + (i * 12) + 8, little));
+      }
+      else if ((marker & 0xFF00) != 0xFF00) break;
+      else offset += view.getUint16(offset, false);
+    }
+    return callback(-1);
+  };
+  reader.readAsArrayBuffer(file.slice(0, 64 * 1024));
+};
 
 function scaleImage(image,maxWidth=500,maxHeight=500){
 	let dst = new cv.Mat();
@@ -89,14 +174,18 @@ function findPaper(image){
 	
 	let maxperi=0;
 	let cindex=0;
-
+	let white=new cv.Scalar(255,255,255);
 	for(let i=0;i<contour.size();++i){
 		let cnt=contour.get(i);
-		let peri = cv.arcLength(cnt, true);
+		//let peri = cv.arcLength(cnt, true);
+		let rotatedRect=cv.minAreaRect(cnt);
+		console.log(rotatedRect.size.width);
+		let peri = rotatedRect.size.width*rotatedRect.size.height;
 		maxperi=Math.max(peri,maxperi);
 		if(peri==maxperi){
 			cindex=i;
 		}
+		cv.drawContours(contourimage,contour,i,white,1,cv.LINE_8,hierachy,0);
 	}
 	let zielkontur=contour.get(cindex);
 	let rotatedRect=cv.minAreaRect(zielkontur);
